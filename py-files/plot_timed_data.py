0e6df57ac4ac4cb784faf924248aa3f2a28b7f2c
Filip Janky <filip.janky@tokamakenergy.co.uk>
2023-10-03 11:26:20 +0100

    Time stepping implemented

--- py-files/plot_timed_data.py
+++ py-files/plot_timed_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt('../data/psi_total_out_1p2.txt_01')
+data = np.loadtxt('../data/psi_total_out_1p2_0.034')
 data = np.resize(data, [65, 33])
 plt.contourf(data)
 plt.colorbar()
