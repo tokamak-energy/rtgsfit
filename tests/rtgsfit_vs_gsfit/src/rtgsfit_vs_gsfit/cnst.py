beb3c2dbc9f601640e49621a155d65d49e183360
aleksyprok <aleksyprok@gmail.com>
2025-07-21 14:31:12 +0100

    Added cnst.py file.

---
+++ tests/rtgsfit_vs_gsfit/src/rtgsfit_vs_gsfit/cnst.py
@@ -0,0 +1,17 @@
+"""
+Python module containing a list of constants used throughout this repository.
+"""
+
+import os
+
+# Github constants
+GSFIT_URL = "https://github.com/tokamak-energy/gsfit.git"
+R_VS_G_SUB_REPO_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+GSFIT_PATH = os.path.join(R_VS_G_SUB_REPO_PATH, "gsfit")
+GSFIT_BRANCH = "rtgsfit_mdsplus"
+
+# MDSplus constants
+PULSE_NUM = 13343
+PULSE_NUM_WRITE = PULSE_NUM + 52_000_000
+RUN_NAME = "RT_V_G_1"
+RUN_DESCRIPTION = "Using a single degree of freedom for p_prime and ff_prime."
