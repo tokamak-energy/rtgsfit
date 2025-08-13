de7e8f9455d5aeeff9cf1d352dd282842124d4a3
aleksyprok <aleksyprok@gmail.com>
2025-08-13 14:34:46 +0100

    Now plotting passive suport ring currents

--- tests/rtgsfit_vs_gsfit/src/rtgsfit_vs_gsfit/plot/examples.py
+++ tests/rtgsfit_vs_gsfit/src/rtgsfit_vs_gsfit/plot/examples.py
@@ -72,25 +72,34 @@ def psi_ivc_passives(cfg: dict, iterations=None):
       • Right: GSFIT results
     """
 
-    from rtgsfit_vs_gsfit.plot.components import ivc_j_rtgsfit, psi_contours
+    from rtgsfit_vs_gsfit.plot.components import ivc_j_gsfit, ivc_j_rtgsfit, passive_j_rtgsfit, \
+        passive_j_gsfit, psi_contours
 
     this_plot_dir = os.path.join(cfg["plots_this_run_dir"], "psi_ivc_passives")
     os.makedirs(this_plot_dir, exist_ok=True)
 
-    iteration = iterations[1]
+    if iterations is None:
+        iterations = np.arange(cfg["n_iters"], dtype=int)
 
-    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+    for iteration in iterations:
 
-    psi_contours(iteration, ax[0], cfg, plot_rtgsfit=True, plot_gsfit=False)
-    psi_contours(iteration, ax[1], cfg, plot_rtgsfit=False, plot_gsfit=True)
-    ivc_j_rtgsfit(iteration, ax[0], cfg)
+        fig, ax = plt.subplots(1, 2, figsize=(12, 6))
 
-    filename = f"psi_ivc_passives_{cfg['pulse_num']}_{cfg['run_name']}_{iteration:02d}.png"
-    fig.savefig(os.path.join(this_plot_dir, filename),
-                bbox_inches='tight',
-                dpi=300)
-    plt.close("all")
-    end_time = time.time()
+        psi_contours(iteration, ax[0], cfg, plot_rtgsfit=True, plot_gsfit=False)
+        psi_contours(iteration, ax[1], cfg, plot_rtgsfit=False, plot_gsfit=True)
+        if iteration != 0:
+            ivc_j_rtgsfit(iteration, ax[0], cfg)
+            passive_j_rtgsfit(iteration, ax[0], cfg)
+        cfg["j_vrange"] = None
+        ivc_j_gsfit(iteration, ax[1], cfg)
+        passive_j_gsfit(iteration, ax[1], cfg)
+
+        filename = f"psi_ivc_passives_{cfg['pulse_num']}_{cfg['run_name']}_{iteration:02d}.png"
+        fig.savefig(os.path.join(this_plot_dir, filename),
+                    bbox_inches='tight',
+                    dpi=300)
+        plt.close("all")
+        end_time = time.time()
 
 if __name__ == "__main__":
 
@@ -99,6 +108,7 @@ if __name__ == "__main__":
     cfg = config_loader.load_and_prepare_config()
 
     iterations = [0, 1, 8]
+    iterations = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     # start_time = time.time()
     # psi_fluxloop_bp(cfg, iterations=iterations)
     # end_time = time.time()
