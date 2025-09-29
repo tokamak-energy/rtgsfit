b137c9fb650b45712427541a1c4df70601c2f565
aleksyprok <aleksyprok@gmail.com>
2025-09-29 14:29:38 +0100

    Removed n_meas_model as an argument from rtgsfit()

--- src/rtgsfit.h
+++ src/rtgsfit.h
@@ -19,7 +19,6 @@ void normalise_flux(double* flux_total, double flux_lcfs,
 void rtgsfit(double *meas, double *coil_curr, double *flux_norm, int32_t *mask,
         double *flux_total, double *chi_sq_err, double *lcfs_r, double *lcfs_z,
         int32_t *lcfs_n, double *coef, double *flux_boundary, double *plasma_current,
-        int32_t *lcfs_err_code, int64_t *lapack_dgelss_info, double *meas_model,
-        int32_t n_meas);
+        int32_t *lcfs_err_code, int64_t *lapack_dgelss_info, double *meas_model);
 
 #endif
