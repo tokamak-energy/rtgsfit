dfc91c528f958448204c67b0faed4f209dc5fa39
aleksyprok <aleksyprok@gmail.com>
2025-10-22 10:18:24 +0100

    Updated the tests in light of lapack_dgelss_info now being 32bit not 64bit (#77)
    * Updated analytic test to check more arguments now.
    
    * Fixed plotting routines in rtgsfit_verify_analytic
    
    * Changed lapack_dgelss_info from 64bit to 32bit
    
    * Now checking lapack_dgelss_info and lcfs_err_code are zero at all iterations.

--- tests/rtgsfit_verify_analytic/tests/test_regression_output.py
+++ tests/rtgsfit_verify_analytic/tests/test_regression_output.py
@@ -1,13 +1,19 @@
+import os
+
 import pytest
 import numpy as np
-from pathlib import Path
 from numpy.testing import assert_allclose
 
 from rtgsfit_verify_analytic import cnst
 
 def test_output_dict_regression(output_dict):
+    """
+    Regression test for the final step only to save storage space.
+    """    
 
-    reference_file = Path(cnst.DATA_DIR) / "reference_output_dict.npy"
+    output_dict_final_iter_path = os.path.join(cnst.DATA_DIR, 'output_dict_final_iter.npy')
+    output_dict_final_iter = np.load(output_dict_final_iter_path, allow_pickle=True).item()
+    reference_file = os.path.join(cnst.DATA_DIR, 'reference_output_dict.npy')
     reference_dict = np.load(reference_file, allow_pickle=True).item()
 
     assert output_dict.keys() == reference_dict.keys(), "Mismatch in output_dict keys"
@@ -16,7 +22,7 @@ def test_output_dict_regression(output_dict):
         rtol = 1e-4
         atol = np.max(np.abs(reference_dict[key])) * rtol
         assert_allclose(
-            output_dict[key][-1],
+            output_dict_final_iter[key],
             reference_dict[key],
             rtol=rtol,
             atol=atol,
--- tests/rtgsfit_vs_gsfit/src/rtgsfit_vs_gsfit/replay_rtgsfit.py
+++ tests/rtgsfit_vs_gsfit/src/rtgsfit_vs_gsfit/replay_rtgsfit.py
@@ -56,17 +56,17 @@ def replay_rtgsfit(cfg: dict):
         ctypes.POINTER(ctypes.c_double),  # meas_pcs
         ctypes.POINTER(ctypes.c_double),  # coil_curr
         ctypes.POINTER(ctypes.c_double),  # flux_norm
-        ctypes.POINTER(ctypes.c_int),     # mask
+        ctypes.POINTER(ctypes.c_int32),   # mask
         ctypes.POINTER(ctypes.c_double),  # flux_total
         ctypes.POINTER(ctypes.c_double),  # chi_sq_err
         ctypes.POINTER(ctypes.c_double),  # lcfs_r
         ctypes.POINTER(ctypes.c_double),  # lcfs_z
-        ctypes.POINTER(ctypes.c_int),     # lcfs_n
+        ctypes.POINTER(ctypes.c_int32),   # lcfs_n
         ctypes.POINTER(ctypes.c_double),  # coef
         ctypes.POINTER(ctypes.c_double),  # flux_boundary
         ctypes.POINTER(ctypes.c_double),  # plasma_current
         ctypes.POINTER(ctypes.c_int32),   # lcfs_err_code
-        ctypes.POINTER(ctypes.c_int64),   # lapack_dgelss_info
+        ctypes.POINTER(ctypes.c_int32),   # lapack_dgelss_info
         ctypes.POINTER(ctypes.c_double),  # meas_model
         ctypes.c_int32,                   # n_meas_model
     ]
@@ -94,7 +94,7 @@ def replay_rtgsfit(cfg: dict):
     mask = mask.flatten().astype(np.int32)
     flux_total = np.zeros(n_grid, dtype=np.float64)
     coef = np.zeros(n_coef, dtype=np.float64)
-    error = np.array([0.0], dtype=np.float64)
+    chi_sq_err = np.array([0.0], dtype=np.float64)
     lcfs_r = np.zeros(n_lcfs_max, dtype=np.float64)
     lcfs_z = np.zeros(n_lcfs_max, dtype=np.float64)
     lcfs_n = np.array([0], dtype=np.int32)
@@ -103,33 +103,41 @@ def replay_rtgsfit(cfg: dict):
     lcfs_err_code = np.array([0], dtype=np.int32)
     lapack_dgelss_info = np.array([0], dtype=np.int64)
     meas_model = np.zeros(n_meas, dtype=np.float64)
+    n_meas_model = np.array([n_meas], dtype=np.int32)
 
     output_dict = {"meas_pcs" : np.zeros((cfg["n_iters"] + 1, n_meas_pcs), dtype=np.float64),
                    "coil_curr" : np.zeros((cfg["n_iters"] + 1, n_coil), dtype=np.float64),
                    "flux_norm" : np.zeros((cfg["n_iters"] + 1, n_grid), dtype=np.float64),
                    "mask" : np.zeros((cfg["n_iters"] + 1, n_grid), dtype=np.int32),
                    "flux_total" : np.zeros((cfg["n_iters"] + 1, n_grid), dtype=np.float64),
-                   "error" : np.zeros((cfg["n_iters"] + 1, 1), dtype=np.float64),
+                   "chi_sq_err" : np.zeros((cfg["n_iters"] + 1), dtype=np.float64),
                    "lcfs_r" : np.zeros((cfg["n_iters"] + 1, n_lcfs_max), dtype=np.float64),
                    "lcfs_z" : np.zeros((cfg["n_iters"] + 1, n_lcfs_max), dtype=np.float64),
-                   "lcfs_n" : np.zeros((cfg["n_iters"] + 1, 1), dtype=np.int32),
+                   "lcfs_n" : np.zeros((cfg["n_iters"] + 1), dtype=np.int32),
                    "coef" : np.zeros((cfg["n_iters"] + 1, n_coef), dtype=np.float64),
                    "flux_boundary" : np.zeros((cfg["n_iters"] + 1, 1), dtype=np.float64),
                    "plasma_current" : np.zeros((cfg["n_iters"] + 1, 1), dtype=np.float64),
-                   "lcfs_err_code" : np.zeros((cfg["n_iters"] + 1, 1), dtype=np.int32)}
+                   "lcfs_err_code" : np.zeros((cfg["n_iters"] + 1, 1), dtype=np.int32),
+                   "lapack_dgelss_info" : np.zeros((cfg["n_iters"] + 1), dtype=np.int32),
+                   "meas_model" : np.zeros((cfg["n_iters"] + 1, n_meas), dtype=np.float64),
+                   "n_meas_model": np.zeros((cfg["n_iters"] + 1), dtype=np.int32),
+                   }
     output_dict["meas_pcs"][0, :] = meas_pcs
     output_dict["coil_curr"][0, :] = coil_curr
     output_dict["flux_norm"][0, :] = flux_norm
     output_dict["mask"][0, :] = mask
     output_dict["flux_total"][0, :] = flux_total
-    output_dict["error"][0, :] = error
+    output_dict["chi_sq_err"][0] = chi_sq_err[0]
     output_dict["lcfs_r"][0, :] = lcfs_r
     output_dict["lcfs_z"][0, :] = lcfs_z
-    output_dict["lcfs_n"][0, :] = lcfs_n
+    output_dict["lcfs_n"][0] = lcfs_n[0]
     output_dict["coef"][0, :] = coef
     output_dict["flux_boundary"][0, :] = flux_boundary
     output_dict["plasma_current"][0, :] = plasma_current
-    output_dict["lcfs_err_code"][0, :] = lcfs_err_code
+    output_dict["lcfs_err_code"][0] = lcfs_err_code[0]
+    output_dict["lapack_dgelss_info"][0] = lapack_dgelss_info[0]
+    output_dict["meas_model"][0, :] = meas_model
+    output_dict["n_meas_model"][0] = n_meas_model[0]
 
     for i_iter in range(cfg["n_iters"]):
 
@@ -141,17 +149,17 @@ def replay_rtgsfit(cfg: dict):
             meas_pcs_copy.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
             coil_curr_copy.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
             flux_norm.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-            mask.ctypes.data_as(ctypes.POINTER(ctypes.c_int)),
+            mask.ctypes.data_as(ctypes.POINTER(ctypes.c_int32)),
             flux_total.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-            error.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            chi_sq_err.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
             lcfs_r.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
             lcfs_z.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-            lcfs_n.ctypes.data_as(ctypes.POINTER(ctypes.c_int)),
+            lcfs_n.ctypes.data_as(ctypes.POINTER(ctypes.c_int32)),
             coef.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
             flux_boundary.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
             plasma_current.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
             lcfs_err_code.ctypes.data_as(ctypes.POINTER(ctypes.c_int32)),
-            lapack_dgelss_info.ctypes.data_as(ctypes.POINTER(ctypes.c_int64)),
+            lapack_dgelss_info.ctypes.data_as(ctypes.POINTER(ctypes.c_int32)),
             meas_model.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
             ctypes.c_int32(n_meas)
         )
@@ -163,14 +171,17 @@ def replay_rtgsfit(cfg: dict):
         output_dict["flux_norm"][i_iter + 1, :] = flux_norm
         output_dict["mask"][i_iter + 1, :] = mask
         output_dict["flux_total"][i_iter + 1, :] = flux_total
-        output_dict["error"][i_iter + 1, :] = error
+        output_dict["chi_sq_err"][i_iter + 1] = chi_sq_err[0]
         output_dict["lcfs_r"][i_iter + 1, :] = lcfs_r
         output_dict["lcfs_z"][i_iter + 1, :] = lcfs_z
-        output_dict["lcfs_n"][i_iter + 1, :] = lcfs_n
+        output_dict["lcfs_n"][i_iter + 1] = lcfs_n[0]
         output_dict["coef"][i_iter + 1, :] = coef
         output_dict["flux_boundary"][i_iter + 1, :] = flux_boundary
         output_dict["plasma_current"][i_iter + 1, :] = plasma_current
-        output_dict["lcfs_err_code"][i_iter + 1, :] = lcfs_err_code
+        output_dict["lcfs_err_code"][i_iter + 1] = lcfs_err_code[0]
+        output_dict["lapack_dgelss_info"][i_iter + 1] = lapack_dgelss_info[0]
+        output_dict["meas_model"][i_iter + 1, :] = meas_model
+        output_dict["n_meas_model"][i_iter + 1] = n_meas_model[0]        
 
     # Save output_dict to a file
     np.save(cfg["rtgsfit_output_dict_path"], output_dict, allow_pickle=True)
