e1be76a8f85548b2633e34375b34e8877de2d9a8
aleksyprok <aleksyprok@gmail.com>
2025-11-04 16:21:27 +0000

    Updated analytic test with new variables in rtgsfit argument list.

--- tests/rtgsfit_verify_analytic/src/rtgsfit_verify_analytic/replay_rtgsfit.py
+++ tests/rtgsfit_verify_analytic/src/rtgsfit_verify_analytic/replay_rtgsfit.py
@@ -46,7 +46,12 @@ def replay_rtgsfit():
         ctypes.POINTER(ctypes.c_int32), # lcfs_err_code
         ctypes.POINTER(ctypes.c_int32), # lapack_dgelss_info
         ctypes.POINTER(ctypes.c_double), # meas_model
-        ctypes.c_int32 # n_meas_model
+        ctypes.c_int32, # n_meas_model
+        ctypes.POINTER(ctypes.c_double), # axis_r
+        ctypes.POINTER(ctypes.c_double), # axis_z
+        ctypes.POINTER(ctypes.c_double), # axis_flux
+        ctypes.POINTER(ctypes.c_double),  # plasma_current_centroid_r
+        ctypes.POINTER(ctypes.c_double)   # plasma_current_centroid_z
     ]
     # Define the return type for the rtgsfit function
     rtgsfit_lib.rtgsfit.restype = None
@@ -99,6 +104,11 @@ def replay_rtgsfit():
     lcfs_err_code = np.array([0], dtype=np.int32)
     lapack_dgelss_info = np.array([0], dtype=np.int32)
     meas_model = np.zeros(n_meas, dtype=np.float64)
+    axis_r = np.array([0.0], dtype=np.float64)
+    axis_z = np.array([0.0], dtype=np.float64)
+    axis_flux = np.array([0.0], dtype=np.float64)
+    plasma_current_centroid_r = np.array([0.0], dtype=np.float64)
+    plasma_current_centroid_z = np.array([0.0], dtype=np.float64)
 
     output_dict = {"meas" : np.zeros((cnst.N_ITERS + 1, n_meas), dtype=np.float64),
                    "coil_curr" : np.zeros((cnst.N_ITERS + 1, n_coil), dtype=np.float64),
@@ -115,7 +125,12 @@ def replay_rtgsfit():
                    "lcfs_err_code" : np.zeros((cnst.N_ITERS + 1), dtype=np.int32),
                    "lapack_dgelss_info" : np.zeros((cnst.N_ITERS + 1), dtype=np.int32),
                    "meas_model" : np.zeros((cnst.N_ITERS + 1, n_meas), dtype=np.float64),
-                   "n_meas_model" : np.zeros((cnst.N_ITERS + 1), dtype=np.int32)}
+                   "n_meas_model" : np.zeros((cnst.N_ITERS + 1), dtype=np.int32),
+                   "axis_r" : np.zeros((cnst.N_ITERS + 1), dtype=np.float64),
+                   "axis_z" : np.zeros((cnst.N_ITERS + 1), dtype=np.float64),
+                   "axis_flux" : np.zeros((cnst.N_ITERS + 1), dtype=np.float64),
+                   "plasma_current_centroid_r" : np.zeros((cnst.N_ITERS + 1), dtype=np.float64),
+                   "plasma_current_centroid_z" : np.zeros((cnst.N_ITERS + 1), dtype=np.float64)}
     output_dict["meas"][0, :] = meas
     output_dict["coil_curr"][0, :] = coil_curr
     output_dict["flux_norm"][0, :] = flux_norm
@@ -131,7 +146,12 @@ def replay_rtgsfit():
     output_dict["lcfs_err_code"][0] = lcfs_err_code[0]
     output_dict["lapack_dgelss_info"][0] = lapack_dgelss_info[0]
     output_dict["meas_model"][0, :] = meas_model
-    output_dict["n_meas_model"][0] = len(meas_model)    
+    output_dict["n_meas_model"][0] = len(meas_model)
+    output_dict["axis_r"][0] = axis_r[0]
+    output_dict["axis_z"][0] = axis_z[0]
+    output_dict["axis_flux"][0] = axis_flux[0]
+    output_dict["plasma_current_centroid_r"][0] = plasma_current_centroid_r[0]
+    output_dict["plasma_current_centroid_z"][0] = plasma_current_centroid_z[0]
 
     for i_iter in range(cnst.N_ITERS):
 
@@ -161,7 +181,12 @@ def replay_rtgsfit():
             lcfs_err_code.ctypes.data_as(ctypes.POINTER(ctypes.c_int32)),
             lapack_dgelss_info.ctypes.data_as(ctypes.POINTER(ctypes.c_int32)),
             meas_model.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-            ctypes.c_int32(len(meas_model))
+            ctypes.c_int32(len(meas_model)),
+            axis_r.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            axis_z.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            axis_flux.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            plasma_current_centroid_r.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            plasma_current_centroid_z.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
         )
 
         print("Iteration:", i_iter + 1)
@@ -182,6 +207,11 @@ def replay_rtgsfit():
         output_dict["lapack_dgelss_info"][i_iter + 1] = lapack_dgelss_info[0]
         output_dict["meas_model"][i_iter + 1, :] = meas_model
         output_dict["n_meas_model"][i_iter + 1] = len(meas_model)
+        output_dict["axis_r"][i_iter + 1] = axis_r[0]
+        output_dict["axis_z"][i_iter + 1] = axis_z[0]
+        output_dict["axis_flux"][i_iter + 1] = axis_flux[0]
+        output_dict["plasma_current_centroid_r"][i_iter + 1] = plasma_current_centroid_r[0]
+        output_dict["plasma_current_centroid_z"][i_iter + 1] = plasma_current_centroid_z[0]
 
         print("Chi-squared error:", chi_sq_err[0])
         print("lapack_dgelss_info:", lapack_dgelss_info[0])
@@ -192,13 +222,6 @@ def replay_rtgsfit():
     np.save(output_file, output_dict, allow_pickle=True)
       
     # Save each in a csv file for visual inspection
-    # In the format:
-    # key0
-    # value0, value1, value2, ...
-    # 
-    # key1
-    # value0, value1, value2, ...
-    # ...
     # Skipping the flux_norm, flux_total, mask, lcfs_r, lcfs_z, lcfs_n for brevity
     csv_file = os.path.join(cnst.DATA_DIR, 'output_dict.csv')
     with open(csv_file, 'w') as f:
