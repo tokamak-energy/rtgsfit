eb323c76af4a9d35be6bad00655f679c58765564
aleksyprok <aleksyprok@gmail.com>
2026-01-05 12:52:07 +0000

    Added new error code for the case where there is no magnetic axis.

--- src/find_x_point.c
+++ src/find_x_point.c
@@ -18,14 +18,15 @@
 #define N_Z_TIMES_2 (N_Z * 2 - 1)
 #define MIN(aa,bb) ((aa)<=(bb)?(aa):(bb))
 #define MAX(aa,bb) ((aa)>=(bb)?(aa):(bb))
-#define ERR_MID_BDRY 1 // 0b00000001
-#define ERR_COL_START 2 // 0b00000010
-#define ERR_COL_END 4 // 0b00000100
-#define ERR_COL_START_END 8 // 0b00001000
-#define ERR_VERT_BDRY 16 // 0b00010000
-#define ERR_MASK_INDEX 32 // 0b00100000
-#define ERR_LCFS_N 64 // 0b01000000
-#define ERR_AX_EQ_BDRY 128 // 0b10000000
+#define ERR_MID_BDRY 1 // 0b000000001
+#define ERR_COL_START 2 // 0b000000010
+#define ERR_COL_END 4 // 0b000000100
+#define ERR_COL_START_END 8 // 0b000001000
+#define ERR_VERT_BDRY 16 // 0b000010000
+#define ERR_MASK_INDEX 32 // 0b000100000
+#define ERR_LCFS_N 64 // 0b001000000
+#define ERR_AX_EQ_BDRY 128 // 0b010000000
+#define ERR_NO_AXIS 256 // 0b100000000
 
 /// Find crossings along the edges of a patch.
 /// 
